1cda0df59a1f72e563fed875f66d3909d58dd5fa
willymariino <wg.mariino@gmail.com>
2025-05-09 18:10:41 +0200

    added 404

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ function show(req, res) {
     const sql = 'SELECT * FROM movies WHERE id = ?';
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: "film non trovato" });
         res.json(results);
     });
 
