3cbe432f5673ee634d94973449c53b8dcae8432f
willymariino <wg.mariino@gmail.com>
2025-05-10 18:31:05 +0200

    added errorsHandler middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const errorsHandler = require("./middlewares/errorsHandler")
 
 // app.get('/', (req, res) => {
 //     res.send('benvenuto nella mia webapp movies') spostate le rotte in routers
