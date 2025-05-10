e6be377300d38f46d309a7f5d557bf41c3d9f111
willymariino <wg.mariino@gmail.com>
2025-05-10 16:54:29 +0200

    imported database in movies

--- routers/movies.js
+++ routers/movies.js
@@ -1,5 +1,6 @@
 const express = require("express") // importiamo express
 const router = express.Router() // mini-router per gestire un gruppo di rotte legate ad una risorsa: (/movies)
+const connection = require('./data/movie_db')
 
 router.get("/", function index(req, res) {
     const sql = 'SELECT * FROM movies';
