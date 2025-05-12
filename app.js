b1b915fe4a088244ffd58315ba39888c5097e97f
willymariino <wg.mariino@gmail.com>
2025-05-12 15:13:11 +0200

    feat: file env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const errorsHandler = require("./middlewares/errorsHandler")
 const notFound = require("./middlewares/notFound")
 
--- data/movie_db.js
+++ data/movie_db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // creiamo l'oggetto di connessione
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'movie_db'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // connettiamoci
