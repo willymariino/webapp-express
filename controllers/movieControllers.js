7679b9fbf6be42214b03c7084c12e70fae0e17ed
willymariino <wg.mariino@gmail.com>
2025-05-13 14:56:00 +0200

    feat: added AVG reviews

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,7 +1,12 @@
 const connection = require('../data/movie_db')
 
 function index(req, res) {
-    const sql = 'SELECT * FROM movies';
+    const sql = `SELECT 
+    books.*, AVG (reviews.vote) AS media_recensioni 
+    FROM movies LEFT JOIN reviews ON movies.id = reviews.movies.id
+    GROUP BY movies.id`;
+
+
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         res.json(results.map(result => ({
