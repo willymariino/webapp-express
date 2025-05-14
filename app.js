f15c85caa546ef487f5256177c900998521b854d
willymariino <wg.mariino@gmail.com>
2025-05-14 18:18:48 +0200

    body parsing

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ const cors = require("cors")
 
 
 // collega tutte le rotte che iniziano con movies
+
 app.use(cors())
+app.use(express.json())
 app.use("/movies", moviesRouter)
 app.use("/", homeRouter)
 app.use("/movies_cover", express.static("movies_cover"))
-app.use(express.json())
+
 
 app.use(errorsHandler)
 app.use(notFound)
